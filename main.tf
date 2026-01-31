79dfcd371125901e32d9c763b3ba99ed47255b1f
AkshayMahajan03 <akshaym4807@gmail.com>
2026-01-31 12:52:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,65 @@
-# Security Group for ALB
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get public subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Security Group for EC2
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["49.37.128.44/32"] # YOUR IP
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  user_data = <<EOF
+#!/bin/bash
+yum install -y httpd
+systemctl start httpd
+echo "Hello from EC2 behind ALB" > /var/www/html/index.html
+EOF
+
+  tags = {
+    Name = "web-ec2"
+  }
+}
+
+# ALB Security Group
 resource "aws_security_group" "alb_sg" {
   name   = "alb-sg"
-  vpc_id = var.vpc_id
+  vpc_id = data.aws_vpc.default.id
 
   ingress {
     from_port   = 80
@@ -20,31 +78,29 @@ resource "aws_security_group" "alb_sg" {
 
 # Application Load Balancer
 resource "aws_lb" "alb" {
-  name               = "simple-alb"
+  name               = "demo-alb"
   load_balancer_type = "application"
-  internal           = false
-
-  security_groups = [aws_security_group.alb_sg.id]
-  subnets         = var.subnet_ids
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = data.aws_subnets.default.ids
 }
 
 # Target Group
 resource "aws_lb_target_group" "tg" {
-  name     = "simple-tg"
+  name     = "demo-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  vpc_id   = data.aws_vpc.default.id
 }
 
 # Attach EC2 to Target Group
-resource "aws_lb_target_group_attachment" "ec2_attach" {
+resource "aws_lb_target_group_attachment" "tg_attach" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = var.instance_id
+  target_id        = aws_instance.web.id
   port             = 80
 }
 
 # Listener
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
