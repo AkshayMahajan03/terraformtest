75af31d6d6f2789868ae8a7148f7e7ef823c1e8b
AkshayMahajan03 <akshaym4807@gmail.com>
2026-01-31 11:49:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "alb_sg" {
+  name   = "alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "simple-alb"
+  load_balancer_type = "application"
+  internal           = false
+
+  security_groups = [aws_security_group.alb_sg.id]
+  subnets         = var.subnet_ids
+}
