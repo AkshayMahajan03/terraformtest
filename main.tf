fb70b889c8a2182789b7f17f20ff6dcc1d6c859b
AkshayMahajan03 <akshaym4807@gmail.com>
2026-01-31 12:16:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Security Group for ALB
 resource "aws_security_group" "alb_sg" {
   name   = "alb-sg"
   vpc_id = var.vpc_id
@@ -17,6 +18,7 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
+# Application Load Balancer
 resource "aws_lb" "alb" {
   name               = "simple-alb"
   load_balancer_type = "application"
@@ -26,9 +28,29 @@ resource "aws_lb" "alb" {
   subnets         = var.subnet_ids
 }
 
+# Target Group
 resource "aws_lb_target_group" "tg" {
   name     = "simple-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+}
+
+# Attach EC2 to Target Group
+resource "aws_lb_target_group_attachment" "ec2_attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = var.instance_id
+  port             = 80
+}
+
+# Listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
 }
