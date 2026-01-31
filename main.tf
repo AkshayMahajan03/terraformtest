b94297b44467a83b4fa41d83f842b308fd177cda
AkshayMahajan03 <akshaym4807@gmail.com>
2026-01-31 12:02:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,3 +25,10 @@ resource "aws_lb" "alb" {
   security_groups = [aws_security_group.alb_sg.id]
   subnets         = var.subnet_ids
 }
+
+resource "aws_lb_target_group" "tg" {
+  name     = "simple-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
